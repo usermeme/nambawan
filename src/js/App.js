75b48b0417c130bdcf0401ed50a44827783c5df9
usermeme <romanyankovich98@gmail.com>
2019-03-18 02:31:23 +0300

    feat: add scroll slider

--- src/js/App.js
+++ src/js/App.js
@@ -3,9 +3,25 @@ import Menu from './menu/Menu';
 export default class App {
   constructor() {
     this.menu = new Menu();
+    this.scroller = document.documentElement.scrollTop;
   }
 
   start() {
+    document.addEventListener('scroll', () => {
+      const newScroll = document.documentElement.scrollTop;
+      const isScrollRange = (this.scroller >= 0 && this.scroller < document.querySelector('#card').offsetTop);
+
+      if (isScrollRange) {
+        if (newScroll > this.scroller) {
+          App.jumpTo('#card');
+        } else {
+          App.jumpTo('#about');
+        }
+      }
+
+      this.scroller = document.documentElement.scrollTop;
+    });
+
     document.addEventListener('click', (e) => {
       let tar = e.target;
 
@@ -17,5 +33,10 @@ export default class App {
         tar = tar.parentNode;
       }
     });
+  }
+
+  static jumpTo(anchor) {
+    const scrollElement = document.querySelector(anchor);
+    scrollElement.scrollIntoView({ behavior: 'smooth' });
   }
 }
